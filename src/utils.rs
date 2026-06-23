65e320511c36776452c972e14be69fff12068520
nachengue007 <nachotdf123@gmail.com>
2026-06-23 19:12:25 -0300

    [UTILS] ojala que sea el definitivo

--- src/utils.rs
+++ src/utils.rs
@@ -1,29 +1,29 @@
-use clipboard_win::{ set_clipboard_string, get_clipboard_string };
-
 pub fn char_to_byte_idx(s: &str, char_idx: usize) -> usize {
   s.char_indices()
     .nth(char_idx)
     .map(|(i, _)| i)
     .unwrap_or_else(|| s.len())
 }
-
+ 
 pub fn line_len_chars(s: &str) -> usize {
   s.chars().count()
 }
-
+ 
 #[cfg(windows)]
 pub fn set_windows_clipboard(text: String) {
+  use clipboard_win::set_clipboard_string;
   let _ = set_clipboard_string(&text);
 }
-
+ 
+#[cfg(not(windows))]
+pub fn set_windows_clipboard(_text: String) {}
+ 
 #[cfg(windows)]
 pub fn get_windows_clipboard() -> Option<String> {
+  use clipboard_win::get_clipboard_string;
   get_clipboard_string().ok()
 }
-
-#[cfg(not(windows))]
-pub fn set_windows_clipboard(text: String) {}
-
+ 
 #[cfg(not(windows))]
 pub fn get_windows_clipboard() -> Option<String> {
   None
